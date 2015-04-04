60b96b99fc0033f973aa59f2bdb21c4e67d6e9eb
Lucas de Freitas Kauer <lucas.kauerrs@hotmail.com>
2015-04-03 22:49:56 -0300

    edd(Orc) - Alterações bobas que não interferem no funcionamento da classe, como: adição e remoção de espaços, alteração de comentários etc. - Implementação de um get que executa o método recebeFlecha e retorna a vida.

--- src/modulo-01-java-OO/Dia01/Orc.java
+++ src/modulo-01-java-OO/Dia01/Orc.java
@@ -4,28 +4,39 @@
  * 
  * @author Lucas Kauer
  */
-public class Orc
-{
+
+public class Orc {
     private int vida ;
+    
     {
         vida = 110;
     }
- 
+    
     /**
      * Construtor para objetos da classe Orc
+     * 
      * public Orc()
      * {
      * }
+     * 
      */
     
     /**
-     * Desconta 10 de vida a cada chamada do método atirarFlecha.
+     * Recebe uma flechada e perde 10 unidades de vida.
      */
+    
     public void recebeFlecha() {
         vida = vida - 10;
     }
     
+    // metódo responsável por testar se o Orc está nascendo com 110 de vida.
     public int getVida() {
         return this.vida;
+    }
+    
+    // metódo responsável por testar se o método recebeFlecha está sendo executado corretamente
+    public int getRecebeFlecha() {
+        recebeFlecha();
+        return vida;
     }
 }
