b73e9ccf56236ba87e294c9ada6e30ff21df6d78
Lucas de Freitas Kauer <lucas.kauerrs@hotmail.com>
2015-04-07 21:57:47 -0300

    edd(Orc): - Adição do import java.util.* para poder criar ArrayList. - Declaração do ArrayList do tipo ItemDoInventario com o nome de item. - Adição de comentários aleatórios. - Remoção do método orcMorto que por hora estava sendo desnecessário. - Alteração do método recebeFlecha (novo recebeAtaque). - Alteração do método gerarNumero. - Criação dos métodos adicionarItem e perderItem + getTamanhoDoInventario.

--- src/modulo-01-java-OO/Dia01/Orc.java
+++ src/modulo-01-java-OO/Dia01/Orc.java
@@ -1,4 +1,4 @@
-
+import java.util.*;
 /**
  * Define o objeto Orc
  * 
@@ -9,6 +9,10 @@ public class Orc {
     private int vida, experiencia;
     private String nome;
     private Status status = Status.VIVO;
+    ArrayList<ItemDoInventario> item = new ArrayList<ItemDoInventario>();
+    
+    // C#
+    // public Status StatusOrc { get; set }
     
     {
         vida = 110;
@@ -24,34 +28,39 @@ public class Orc {
     
     public Orc() {
     }
-    
-    public void orcMorto() {
-        if(this.vida == 0) {
-            this.status = Status.MORTO;
-        }
-    }
-    
+           
     /**
      * Faz o Orc sofrer um ataque
      * Atualmente 10 de dano será decrementado
      */
     
-    public void recebeFlecha() {
-        if (vida>0){
-                double numeroGerado = gerarNumero();
-                    if (numeroGerado < 0){
-                        this.experiencia += 2;
-                        return;
-                    }else if (numeroGerado > 100) {
-                        int danoVida = 10;
-                        if (this.vida >= danoVida) {
-                            this.vida -= danoVida;
-                            this.status = Status.FERIDO;
-                        }
-                    }
-            }else {
-                this.status = Status.MORTO;
+    public void recebeAtaque() {
+        // Caso o resultado seja menor que 0, o Orc não deverá receber a flecha e ainda
+        // ganhará 2 pontos de experiência.
+        // Senão se o número estiver entre 0 e 100 o Orc não recebe flechas e não recebe experiência. Caso contrário o Orc
+        // receberá a flechada (como está hoje). 
+        if (vida > 0) {
+            
+            double numeroGerado = gerarNumero();
+            
+            if (numeroGerado < 0) {
+                this.experiencia += 2;
+                return;
+            } else if (numeroGerado > 100) {
+                // FLUXO ATUAL:
+            
+                int danoVida = 10;
+                
+                if (this.vida >= danoVida) {
+                    this.vida -= danoVida;
+                    // this.vida = this.vida - 10;
+                    this.status = Status.FERIDO;
+                }
             }
+        }
+        else {
+            this.status = Status.MORTO;
+        }       
     }
     
     // metódo responsável por retornar o nome do Orc
@@ -79,12 +88,12 @@ public class Orc {
         return this.status;
     }
     
-    public void setStatus(Status novoStatus) {
+    /* public void setStatus(Status novoStatus) {
         this.status = novoStatus;
-    }
+    } */
     
-    public void setExperiencia(int experiencia) {
-        this.experiencia = experiencia;
+    public void setExperiencia(int novaExperiencia) {
+        this.experiencia = novaExperiencia;
     }
     
     /**
@@ -99,47 +108,72 @@ public class Orc {
     }
     
     private double gerarNumero() {
+        
         double numeroGerado = 0.0;
         
         // A. Se o orc possuir nome e o mesmo tiver mais de 5 letras, some 65 ao número.
         // Caso contrário, subtraia 60.
-        boolean possuiNomeComMaisDe5Caracteres = (this.nome != null && this.nome.length() > 5);
-        if(possuiNomeComMaisDe5Caracteres) {
+        boolean possuiNomeComMaisDe5Caracteres =
+            this.nome != null && this.nome.length() > 5;
+            
+        if (possuiNomeComMaisDe5Caracteres) {
             numeroGerado += 65;
         } else {
             numeroGerado -= 60;
         }
         
-        // B. Se o orc possuir vida entre 30 e 60, multiple o número por dois,
-        // senão se a vida for menor que 20 multiplique por 3.
-        boolean possuiVidaEntre30e60 = (this.vida >= 30 && this.vida <=60);
+        // B. Se o orc possuir vida entre 30 e 60, multiple o número por dois, 
+        // senão se a vida for menor que 20 multiplique por 3. 
+        boolean possuiVidaEntre30e60 =
+            this.vida >= 30 && this.vida <= 60;
+        //boolean vidaMenorQue20 = this.vida < 20;
+            
         if (possuiVidaEntre30e60) {
             numeroGerado *= 2;
         } else if (this.vida < 20) {
             numeroGerado *= 3;
         }
         
-        // C. Se o orc estiver fugindo, divida o número por 2. Senão se o orc estiver caçando
-        // ou dormindo adicione 1 ao número.
-        boolean estaFugindo = (this.status == Status.FUGINDO);
-        boolean estaCacandoOuDormindo = ((this.status == Status.CAÇANDO) || (this.status == Status.DORMINDO));
+        // Se o orc estiver fugindo, divida o número por 2.
+        // Senão se o orc estiver caçando ou dormindo adicione 1 ao número. 
+        boolean estaFugindo = this.status == Status.FUGINDO;
+        boolean estaCaçandoOuDormindo =
+            this.status == Status.CAÇANDO || this.status == Status.DORMINDO;
+        
         if (estaFugindo) {
             numeroGerado /= 2;
-        } else if (estaCacandoOuDormindo){
+            // #xatiado
+        } else if (estaCaçandoOuDormindo) {
             numeroGerado += 1;
         }
         
-        // D. Se a experiência do orc for par, eleve o número ao cubo. 
+        // Se a experiência do orc for par, eleve o número ao cubo.
         // Se for ímpar e o orc tiver mais que 2 de experiência, eleve o número ao quadrado.
-        boolean experienciaEPar = this.experiencia % 2 == 0;
-        if (experienciaEPar) {
-            // numeroGerado = numeroGerado * numeroGerado * numeroGerado;
-            Math.pow(numeroGerado, 3);
-        } else if (this.experiencia > 2){
-            // numeroGerado = numeroGerado * numeroGerado;
-            Math.pow(numeroGerado, 2);
+        boolean experienciaÉPar = this.experiencia % 2 == 0;
+        
+        if (experienciaÉPar) {
+            numeroGerado = numeroGerado * numeroGerado * numeroGerado;
+            // numeroGerado = Math.pow(numeroGerado, 3);
+        } else if (this.experiencia > 2) {
+            numeroGerado = Math.pow(numeroGerado, 2);
         }
         
         return numeroGerado;
+    }
+    
+    // Tema: Permitir que Orcs ganhem e percam itens (ArrayList)
+    // Método para adicionar itens ao inventário.
+    public void adicionarItem(ItemDoInventario i) {
+        item.add(i);
+    }
+    // Método para remover itens ao inventário.
+    public void perderItem(ItemDoInventario i) {
+        if(item.size() > 0){
+            item.remove(i);
+        }
+    }
+    
+    public int getTamanhoDoInventario() {
+        return item.size();
     }
 }
