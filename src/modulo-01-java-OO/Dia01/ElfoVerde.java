619c9999f5cca541c7cebedd5a3c268b6ea25843
Lucas de Freitas Kauer <lucas.kauerrs@hotmail.com>
2015-04-14 15:52:20 -0300

    create(ElfoVerde) - Adicionado o método toString.

--- src/modulo-01-java-OO/Dia01/ElfoVerde.java
+++ src/modulo-01-java-OO/Dia01/ElfoVerde.java
@@ -42,5 +42,10 @@ public class ElfoVerde extends Elfo {
         if (EEspadaDeAcoValirianoOuArcoEFlechaDeVidro) {
             super.adicionarItem(i);
         }
+    } 
+    
+    @Override
+    public String toString() {
+        return "Elfo Verde: " + super.toString();
     }
 }
