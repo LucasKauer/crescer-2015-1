0fb50d1bb6c245a0dca3483ab9bb72207a34db7b
Lucas de Freitas Kauer <lucas.kauerrs@hotmail.com>
2015-04-18 19:02:14 -0300

    alt(NaoPodeAlistarException) - Alteração na documentação e na mensagem passada no construtor.

--- src/modulo-01-java-OO/Dia01/NaoPodeAlistarException.java
+++ src/modulo-01-java-OO/Dia01/NaoPodeAlistarException.java
@@ -1,11 +1,11 @@
 
 /**
- * Escreva a descrição da classe NaoPodeAlistarException aqui.
+ * Define o erro lançado quando um elfo não pode ser alistado.
  * 
  * @author Lucas Kauer
  */
 public class NaoPodeAlistarException extends Exception {
     public NaoPodeAlistarException() {
-        super("Erro ao alistar um Elfo. Por favor, tente novamente e se o error persistir contate o Administrador!");
+        super("Erro ao alistar um Elfo. Por favor tente novamente e se o error persistir contate o Adminstrador.");
     }
 }
