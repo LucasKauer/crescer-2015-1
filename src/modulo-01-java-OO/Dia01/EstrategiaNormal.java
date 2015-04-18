1143e1f9b26c4e3e2fd686c17288ff59400c857e
Lucas de Freitas Kauer <lucas.kauerrs@hotmail.com>
2015-04-18 19:00:58 -0300

    alt(EstrategiaNormal e EstrategiaNoturna) - Adição e alteração na documentação de ambas as classes. - Implementação de um método novo em ambas as classes.
    Ps.: Todos os testes estão rodando com êxito até o presente commit. ;DD

--- src/modulo-01-java-OO/Dia01/EstrategiaNormal.java
+++ src/modulo-01-java-OO/Dia01/EstrategiaNormal.java
@@ -1,17 +1,28 @@
 import java.util.*;
 /**
- * Escreva a descrição da classe EstrategiaNormal aqui.
+ * Define a Estrategia Normal de ataque dos Elfos.
  * 
  * @author Lucas Kauer
  */
 public class EstrategiaNormal implements EstrategiaDeAtaque {
+    private ArrayList<Elfo> ordemAtaque = new ArrayList<>();
+    
+    /**
+     * Cada Elfo atira em toda a horda de Orcs
+     * @param umElfo Elfo que atacará a horda inteira de Orcs
+     */
     public void atacarOrcs(ArrayList<Elfo> elfos, ArrayList<Orc> orcs) {
        System.out.println("Estratégia Normal");
        
        for (Elfo umElfo : elfos) {
+           ordemAtaque.add(umElfo);
            for (Orc umOrc : orcs) {
                umElfo.atiraFlecha(umOrc);
             }
         }
+    }
+    
+    public ArrayList<Elfo> getOrdemDoUltimoAtaque() {
+        return ordemAtaque;
     }
 }
--- src/modulo-01-java-OO/Dia01/EstrategiaNoturna.java
+++ src/modulo-01-java-OO/Dia01/EstrategiaNoturna.java
@@ -1,10 +1,18 @@
 import java.util.*;
 /**
- * Escreva a descrição da classe EstrategiaNoturna aqui.
+ * Define a Estrategia Noturna de ataque dos Elfos
+ * Diferente da Estrategia Normal a Estrategia Noturna preserva a vida dos seus Elfos Noturnos ;D
  * 
  * @author Lucas Kauer
  */
 public class EstrategiaNoturna implements EstrategiaDeAtaque {
+    private ArrayList<Elfo> ordemAtaque = new ArrayList<>();
+    
+    /**
+     * Cada Elfo atira em toda a horda de Orcs
+     * Porém, os elfos noturno pode ser usados, no máximo, em 30% dos ataques.
+     * @param umElfo Elfo que atacará a horda inteira de Orcs
+     */
     public void atacarOrcs(ArrayList<Elfo> elfos, ArrayList <Orc> orcs) {
         System.out.println("Estratégia Noturna");
         
@@ -19,12 +27,16 @@ public class EstrategiaNoturna implements EstrategiaDeAtaque {
                 if (qtdElfosNoturnosQueJaAtacaram >= limiteElfosNoturnos)
                     continue;
                 qtdElfosNoturnosQueJaAtacaram++;
+            ordemAtaque.add(elfoQueVaiAtacar);
             }
             
             for (Orc orc : orcs) {
                 elfoQueVaiAtacar.atiraFlecha(orc);
             }
-            
         }
+    }
+    
+    public ArrayList<Elfo> getOrdemDoUltimoAtaque() {
+        return ordemAtaque;
     }
 }
