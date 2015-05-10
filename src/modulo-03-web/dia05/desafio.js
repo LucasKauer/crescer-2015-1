04d443b3055f695dc85e9781e1e703898f400d9b
Lucas de Freitas Kauer <lucas.kauerrs@hotmail.com>
2015-05-10 10:02:54 -0300

    add(desafio) - Resolução total do desafio do dia 05.

--- src/modulo-03-web/dia05/desafio.js
+++ src/modulo-03-web/dia05/desafio.js
@@ -91,3 +91,50 @@ function somarPorNacionais(arr) {
 		});
 	return soma;
 }
+
+// 3.B
+function somarPorContinentais(arr) {
+	var soma = 0;
+	var continentais = arr.map(
+		function(i) {
+			return i.titulos[1].qtd;
+		});
+	continentais.forEach(
+		function(valorAtual) {
+			soma += valorAtual;
+		});
+	return soma;
+}
+
+// 3.C
+var somaPorTodosTitulos = function(arr) {		
+		var soma = 0;
+		arr.map(function(i) {
+			soma += i.titulos[0].qtd + i.titulos[1].qtd + i.titulos[2].qtd;
+		});	
+	return soma;
+}
+	
+// 4
+function apenasOsMelhores(arr) {
+	var arrayApenasOsMelhores = arr.map(function(i) {
+		var ehUmDosMelhores = i.titulos[0].qtd > 18;
+		if (ehUmDosMelhores) {
+			return i;
+		}
+	});
+	return arrayApenasOsMelhores;
+}
+
+// 5.A
+function apenasOsMelhoresParaJSON(arr) {
+	var ApenasOsMelhoresParaJSON = apenasOsMelhores(arr);
+	return JSON.stringify(ApenasOsMelhoresParaJSON);
+}
+
+// 5.B
+var strRepresentacaoJSONApenasOsMelhores = apenasOsMelhoresParaJSON(clubes_ingleses);
+function apenasOsMelhoresRecebendoJSON(str) {
+	var arrayApenasOsMelhores = JSON.parse(str);
+	return arrayApenasOsMelhores;
+}
