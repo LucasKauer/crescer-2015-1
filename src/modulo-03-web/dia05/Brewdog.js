6954af026b5ff19d04ca2bd78ff734c865250bcb
Lucas de Freitas Kauer <lucas.kauerrs@hotmail.com>
2015-05-10 14:52:08 -0300

    create(js-part_II) - Feitos os exercícios do Brewdog e Emprestimo.

---
+++ src/modulo-03-web/dia05/Brewdog.js
@@ -0,0 +1,31 @@
+// 1. Brewdog
+function brewdog(strA, strB) {
+	var numeroCaracteresDiferentes = 0;
+	var strMaior = strA.length > strB.length ? strA.length : strB.length;
+	
+	for(var i = 0; i < strMaior; i++) {
+		var caracteresSaoIguais = (strA.charAt(i) != strB.charAt(i));
+		if (caracteresSaoIguais) {
+			numeroCaracteresDiferentes++;
+		}
+	}
+	return numeroCaracteresDiferentes;
+}
+
+console.log(brewdog('bacon', 'beico')); // 4
+
+[
+    { params: ['american ipa','american ipa'],                        esperado: 0 },
+    { params: ['russian imperial stout','russian imperial vodka'],    esperado: 5 },
+    { params: ['1002012','dfgdf12'],                                  esperado: 5 },
+    { params: ['k4rol1n','k4thr1n'],                                  esperado: 3 },
+    { params: ['a1df222266%334op;','ssdfjfdkjgsdfgofdgudf9g8dufsg'],  esperado: 26 },
+  ].forEach(
+    function(ct) {
+      var res = brewdog(ct.params[0], ct.params[1]);
+      console.assert(res === ct.esperado, 'Errooooou! CT:', ct.params, 'Esperado:', ct.esperado, 'Obtido:', res);
+    }
+  );
+  
+ // 2. Emprestimo
+ 
---
+++ src/modulo-03-web/dia05/Emprestimo.js
@@ -0,0 +1,40 @@
+//2.B
+console.log(emprestimo.valorParcela()); // 269.03
+
+/* 2. Emprestimo */
+function Emprestimo(valorTotal, taxaDeJuros, quantidadeParcelas) {
+	
+	// 2.A
+	this.valorTotal = valorTotal;
+	this.taxaDeJuros = taxaDeJuros;
+	this.quantidadeParcelas = quantidadeParcelas || 3;
+	
+	// 2.B
+	this.valorParcela = function() {
+		
+		var valor = (this.valorTotal * this.taxaDeJuros) / (1 - (1 / Math.pow((1 + this.taxaDeJuros), this.quantidadeParcelas)));
+		return parseFloat(valor.toFixed(2));
+	}
+}
+
+// 2.A
+var emprestimo = new Emprestimo(1000, 0.03, 4);
+console.log(emprestimo.valorTotal); // 1000
+
+var emprestimo2 = new Emprestimo(500, 0.05);
+console.log(emprestimo2.quantidadeParcelas); // 3
+
+// 2.B
+console.log(emprestimo.valorParcela()); // 269.03
+
+/* Script para testar a funcao valorParcela */
+[
+    { emprestimo: new Emprestimo(1000, 0.03, 4),  esperado: 269.03 },
+    { emprestimo: new Emprestimo(1800, 0.05, 8),  esperado: 278.5 },
+    { emprestimo: new Emprestimo(500, 0.05),      esperado: 183.6 },
+].forEach(
+    function(ct) {
+      var res = ct.emprestimo.valorParcela();
+      console.assert(res === ct.esperado, 'Errooooou! CT:', ct.emprestimo, 'Esperado:', ct.esperado, 'Obtido:', res);
+    }
+);
