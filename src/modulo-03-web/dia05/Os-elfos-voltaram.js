7992184b0c34c487d2e61635cecc86e2b2063713
Lucas de Freitas Kauer <lucas.kauerrs@hotmail.com>
2015-05-11 14:45:05 -0300

    Estou começando a gostar dos Elfos :3

--- src/modulo-03-web/dia05/Os-elfos-voltaram.js
+++ src/modulo-03-web/dia05/Os-elfos-voltaram.js
@@ -1,21 +1,23 @@
-function Elfo (nome, flechas) {
+/* Avisa o JS para ele ser mais rigoroso e lancar erro para as besteiras do JS_10_dias */
+'use strict';
+
+a = 10;
+console.log(a);
+
+/* function Elfo (nome, flechas) {
 
 	this.nome = nome;
 	this.flechas = flechas || 42;
 	this.experiencia = 0;
 	
-	/*	Duplica "objetos" (funcoes) em memória
-		this.atirarFlecha = function() {
-		this.flechas--;
-		this.experiencia++;
-	}*/
-}
+	// Duplica "objetos" (funcoes) em memória
+	// this.atirarFlecha = function() {
+	// this.flechas--;
+	// this.experiencia++;
+	// }
+} */
 
 /* Jeitinho maroto de não replicar código em memória */
-Elfo.prototype.atirarFlecha = function() {
-	this.flechas--;
-	this.experiencia++;
-}
 
 function Elfo (options) {
 
@@ -24,10 +26,14 @@ function Elfo (options) {
 	this.flechas = options.flechas || 42;
 	this.experiencia = 0;
 	
-	/*	Duplica "objetos" (funcoes) em memória
-		this.atirarFlecha = function() {
-		this.flechas--;
-		this.experiencia++;
-	} */
-	
+	// Duplica "objetos" (funcoes) em memória
+	// this.atirarFlecha = function() {
+	// this.flechas--;
+	// this.experiencia++;
+	// }
+}
+
+Elfo.prototype.atirarFlecha = function() {
+	this.flechas--;
+	this.experiencia++;
 }
